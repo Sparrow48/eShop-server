e361af3864b36858b5ccffbf71f39378abeeb04f
Sparrow48 <abunasib48@gmail.com>
2023-03-14 08:13:27 +0600

    Setup eShop server.

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const corsOptions = {
+    origin: '*',
+    Credential: true,
+    optionSuccessStatus: 200
+}
+
+const app = express()
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(morgan('tiny'))
+dotenv.config({ path: '.env' })
+const PORT = process.env.PORT || 5001
+
+app.get('/', async (req, res) => {
+    res.send('Welcome to eShop server.')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}.`);
+})
