f67eab6d1f29260f23003c5e28aea4d98514cafe
Sparrow48 <abunasib48@gmail.com>
2023-03-14 08:34:17 +0600

    Setup of mongoDB

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const connectDB = require('./server/database/connection')
+
 const corsOptions = {
     origin: '*',
     Credential: true,
@@ -15,6 +17,9 @@ app.use(cors(corsOptions))
 app.use(morgan('tiny'))
 dotenv.config({ path: '.env' })
 const PORT = process.env.PORT || 5001
+
+//MongoDB connection
+connectDB()
 
 app.get('/', async (req, res) => {
     res.send('Welcome to eShop server.')
