137e105ca3cdc0aa8d22b03207524aac5a31079b
Sparrow48 <abunasib48@gmail.com>
2023-05-07 13:20:04 +0600

    Update edit profile API integration.

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const corsOptions = {
 }
 
 const app = express()
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(morgan('tiny'))
